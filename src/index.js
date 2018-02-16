d9178a3d1b1b553a70699e4393aec6f308002ecf
Georgiy Ivanov <georgiy.work@yandex.ru>
2018-02-16 10:45:29 +0500

    Add hot-reloading in dev and update eslint settings to prettier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById("root");
+
+ReactDOM.render(<App />, rootEl);
 registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    const NextApp = require("./App").default;
+    ReactDOM.render(<NextApp />, rootEl);
+  });
+}
